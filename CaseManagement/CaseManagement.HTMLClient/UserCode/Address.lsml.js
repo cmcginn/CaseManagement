275268fee03f8ebcdc9db7052cb286276083dc9a
Chris McGinn <chris.s.mcginn@live.com>
2014-04-07 15:12:38 -0400

    Deleted HTML Client
    Seems to only be useful for small scoped mobile applications

--- CaseManagement/CaseManagement.HTMLClient/UserCode/Address.lsml.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
-
-myapp.Address.created = function (entity) {
-    // Write code here.
-    entity.Id = "00000000-0000-0000-0000-000000000000";
-};
--- CaseManagement/CaseManagement.HTMLClient/UserCode/Person.lsml.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
-
-myapp.Person.created = function (entity) {
-    // Write code here.
-    entity.Id = "00000000-0000-0000-0000-000000000000";
-};
