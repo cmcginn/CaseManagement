6d4687a3ceea669088aa5dce11339c43a5e44dfa
Chris McGinn <chris.s.mcginn@live.com>
2014-04-07 08:28:38 -0400

    Work in PRogress

---
+++ CaseManagement/CaseManagement.HTMLClient/UserCode/Person.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.Person.created = function (entity) {
+    // Write code here.
+    entity.Id = "00000000-0000-0000-0000-000000000000";
+};
---
+++ CaseManagement/CaseManagement.HTMLClient/UserCode/c_Case.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.c_Case.created = function (entity) {
+    // Write code here.
+    entity.Id = "00000000-0000-0000-0000-000000000000";
+    entity.CreatedOnUtc = new Date();
+    entity.LastUpdatedOnUtc = new Date();
+    entity.Address = new myapp.Address();
+    entity.Person = new myapp.Person();
+};
