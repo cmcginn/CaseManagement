bb67bffdcc2077f81ca83ee2b7061e69cf337d09
Chris McGinn <chris.s.mcginn@live.com>
2014-04-12 15:10:49 -0400

    Contact Detail Work in Progress

--- CaseManagement/CaseManagement.DesktopClient/Screens/ContactDetail.lsml.cs
+++ CaseManagement/CaseManagement.DesktopClient/Screens/ContactDetail.lsml.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Collections.Generic;
+using System.Windows.Controls;
 using Microsoft.LightSwitch;
 using Microsoft.LightSwitch.Framework.Client;
 using Microsoft.LightSwitch.Presentation;
@@ -12,7 +13,17 @@ namespace LightSwitchApplication
 {
     public partial class ContactDetail
     {
-      
+        void SetContactAddressProperties()
+        {
+            var primaryAddress = ContactAddresses.FirstOrDefault(x => x.Primary);
+            if (primaryAddress != null)
+                this.PrimaryAddress = primaryAddress.Address;
+            ContactAddresses.ToList().ForEach(x =>
+            {
+                this.FindControlInCollection("SetPrimaryAddress", x).IsVisible = x.Primary == false;
+
+            });
+        }
         partial void ContactDetail_Created()
         {
             if (ContactId.HasValue)
@@ -23,6 +34,7 @@ namespace LightSwitchApplication
             //else
 
             this.SetDisplayNameFromEntity(this.ScreenData);
+            SetContactAddressProperties();
 
         }
 
@@ -37,6 +49,47 @@ namespace LightSwitchApplication
             // Write your code here.
             this.OpenModalWindow("AddressSearch");
         }
+
+        partial void AddressesAddAndEditNew_CanExecute(ref bool result)
+        {
+            // Write your code here.
+
+        }
+
+        partial void AddressesAddAndEditNew_Execute()
+        {
+            // Write your code here.
+            this.NewAddress = this.Addresses.AddNew();
+            this.OpenModalWindow("CreateNewAddress");
+        }
+
+        partial void SaveAddress_Execute()
+        {
+            // Write your code here.
+            this.CloseModalWindow("CreateNewAddress");
+        }
+
+        partial void SelectAddress_Execute()
+        {
+        
+            if (!this.ScreenData.ContactAddresses.Any(x => x.Id == this.Addresses.SelectedItem.Id))
+            {
+
+                var newContactAddress  = this.ScreenData.ContactAddresses.AddNew();
+                newContactAddress.Address = this.Addresses.SelectedItem;
+
+            }
+        }
+
+        partial void SetPrimaryAddress_Execute()
+        {
+            // Write your code here.
+            this.ContactAddresses.Where(x => x.Primary).ToList().ForEach(x => x.Primary = false);
+            this.ContactAddresses.SelectedItem.Primary = true;
+            SetContactAddressProperties();
+        }
+
+
 
         
     }
